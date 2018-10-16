c0bbfd79bc1286204e9b73f6ca5c7b436a3bd22b
Ken <ken@spiretrading.com>
2018-10-16 13:53:46 -0400

    Added subtraction and multiplication by scalar tests

--- Source/Tests/MatrixTester.cpp
+++ Source/Tests/MatrixTester.cpp
@@ -18,3 +18,45 @@ TEST_CASE("Testing addition of Matrices", "[operator+]") {
     {14, 16, 18});
   REQUIRE(mat1 + mat2 == result);
 }
+
+TEST_CASE("Testing subtraction of Matrices", "[operator-]") {
+  auto mat1 = Matrix(
+    {2, 4, 6},
+    {8, 10, 12},
+    {14, 16, 18});
+  auto mat2 = Matrix(
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9});
+  auto result1 = Matrix(
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9});
+  REQUIRE(mat1 - mat2 == result1);
+  auto mat3 = Matrix(
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9});
+  auto mat4 = Matrix(
+    {2, 4, 6},
+    {8, 10, 12},
+    {14, 16, 18});
+  auto result2 = Matrix(
+    {-1, -2, -3},
+    {-4, -5, -6},
+    {-7, -8, -9});
+  REQUIRE(mat3 - mat4 == result2);
+}
+
+TEST_CASE("Testing multiplication of Matrix and scalar", "[operator*]") {
+  auto scalar = 2;
+  auto mat = Matrix(
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9});
+  auto result = Matrix(
+    {2, 4, 6},
+    {8, 10, 12},
+    {14, 16, 18});
+  REQUIRE(scalar * mat == result);
+}

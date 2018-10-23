a38092b999ab043001360f0a4bf1ce0154291ebe
Ken <ken@spiretrading.com>
2018-10-23 18:07:54 -0400

    Added failing test for make_line()

---
+++ Source/Tests/MakeShapeTester.cpp
@@ -0,0 +1,15 @@
+#include <catch2/catch.hpp>
+#include "Ashkal/Ashkal/Matrix.hpp"
+#include "Ashkal/Ashkal/Point.hpp"
+#include "Ashkal/Shapes/Line.hpp"
+#include "Tests/TestUtility.hpp"
+
+using namespace Ashkal;
+
+TEST_CASE("Testing Creation of Line from Points", "[translate]") {
+  auto p1 = Point{-2, 2};
+  auto p2 = Point{2, -2};
+  auto line = make_line(p1, p2);
+  auto result = Matrix();
+  REQUIRE(equals(line.getTransformation(), result));
+}

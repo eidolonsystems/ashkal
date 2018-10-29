45dc470ee2f27c789bdcdcefa305cb7c306f745e
Ken <ken@spiretrading.com>
2018-10-29 18:33:57 -0400

    Re-added PI constant to angle calculation

--- Source/Shapes/Line.cpp
+++ Source/Shapes/Line.cpp
@@ -13,7 +13,7 @@ Line Ashkal::make_line(const Point& p1, const Point& p2) {
     std::pow(std::abs(p1.y - p2.y), 2));
   auto mid_point = Point{(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
   auto slope = (p1.y - p2.y) / (p1.x - p2.x);
-  auto angle = std::abs(atan(slope) - atan(1));
+  auto angle = PI - std::abs(atan(slope) - atan(1));
   auto a = rotate(angle);
   auto b = scale(length / std::sqrt(2));
   auto c = translate(mid_point.x, mid_point.y);

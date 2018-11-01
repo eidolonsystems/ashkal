00ff372cbbe6ab25e918ecaa40a74dafaeb22891
Ken <ken@spiretrading.com>
2018-11-01 17:45:37 -0400

    Updated operator*(Matrix, Point) to use Matrix row * vector column format

--- Source/Common/Point.cpp
+++ Source/Common/Point.cpp
@@ -3,8 +3,8 @@
 using namespace Ashkal;
 
 Point Ashkal::operator *(const Matrix& mat, const Point& p) {
-  return Point{p.x * mat.get(0, 0) + p.x * mat.get(0, 1) + mat.get(0, 2),
-    p.y * mat.get(1, 0) + p.y * mat.get(1, 1) + mat.get(1, 2)};
+  return Point{p.x * mat.get(0, 0) + p.y * mat.get(0, 1) + mat.get(0, 2),
+    p.x * mat.get(1, 0) + p.y * mat.get(1, 1) + mat.get(1, 2)};
 }
 
 double Ashkal::distance(const Point& p1, const Point& p2) {
